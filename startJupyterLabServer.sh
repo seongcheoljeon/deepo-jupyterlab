5867bd017c2d2f162a0ef46e86d427c59dd9164d
Seongcheol Jeon <saelly55@gmail.com>
2020-11-30 22:53:38 +0900

    notebook 디렉토리가 존재하지 않을 시, 디렉토리 생성

--- startJupyterLabServer.sh
+++ startJupyterLabServer.sh
@@ -4,8 +4,10 @@
 # created date: 2020.11.30
 
 
+[[ ! -d /data/notebook ]] && mkdir -p /data/notebook
+
 # bash 쉘에서 실행할 때는 아래의 명령을 사용하면 된다.
 # /usr/bin/nohup /usr/local/bin/jupyter lab --ip 0.0.0.0 --port 8889 --allow-root --no-browser --notebook-dir=/data --NotebookApp.token='' --NotebookApp.password='' &
 
 # dockerfile에서 실행할 때는 아래의 명령을 사용하면 된다.
-/usr/local/bin/jupyter lab --ip 0.0.0.0 --port 8889 --allow-root --no-browser --notebook-dir=/data --NotebookApp.token='' --NotebookApp.password=''
+/usr/local/bin/jupyter lab --ip 0.0.0.0 --port 8889 --allow-root --no-browser --notebook-dir=/data/notebook --NotebookApp.token='' --NotebookApp.password=''
